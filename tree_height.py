5a2d67b5714ff9e24a99c442564a3920ed21aba5
Mārtiņš Evarts Zviedris <125508859+MartinsEvartsZviedris221RDB210@users.noreply.github.com>
2023-03-05 20:58:04 +0200

    Update tree_height.py

--- tree_height.py
+++ tree_height.py
@@ -2,62 +2,56 @@ import sys
 import threading
 import os
 
-
-def compute_height(num_nodes, parents):
-    
-    children = {i: [] for i in range(num_nodes)}
+def compute_height(n, parents):
+    children = {i: [] for i in range(n)}
 
     root = None
-    for i in range(num_nodes):
-        
+    for i in range(n):
         parent = parents[i]
-        
         if parent == -1:
-            root =i
-            
+            root = i
         else:
             children[parent].append(i)
 
     def height(node):
-        
         if not children[node]:
             return 1
-        
         else:
-            return 1 +max(height(child) for child in children[node])
-
+            return 1 + max(height(child) for child in children[node])
     return height(root)
 
-
 def main():
-    
-    input_type = input("Enter 'F' for file, or 'I' for input: ")
-    
-    if "I" in input_type:
-        num_nodes = int(input("Enter the number of nodes: "))
-        parents = list(map(int, input("Enter the parent nodes devided by spaces: ").split()))
-        
-    elif "F" in input_type:
+    text = input()
+    if "I" in text:
+        n = int(input())
+        parents = list(map(int, input().split()))
+    elif "F" in text:
+
         path = "./test/"
-        filename = input("Enter the name of file: ")
+
+        filename = input("Enter the file name: ")
+
         file_path = os.path.join(path, filename)
 
         if "a" not in filename:
             try:
                 with open(file_path) as f:
-                    num_nodes = int(f.readline().strip())
+                    n = int(f.readline().strip())
                     parents = list(map(int, f.readline().strip().split()))
-                    
             except Exception as e:
                 print("Error:", str(e))
                 return
-            
+
     else:
-        print("Invalid input type. Please enter 'I' or 'F'.")
+        print("Enter 'I' or 'F':")
         return
 
-    print(compute_height(num_nodes, parents))
-    
+    print(compute_height(n, parents))
+
+
+# In Python, the default limit on recursion depth is rather low,
+# so raise it here for this problem. Note that to take advantage
+# of bigger stack, we have to launch the computation in a new thread.
 sys.setrecursionlimit(107)  # max depth of recursion
-threading.stack_size(227)  # new thread will get stack of such size
+threading.stack_size(227)   # new thread will get stack of such size
 threading.Thread(target=main).start()

ed15db5354a2ca6d26d601319db01b3588a093ec
Mārtiņš Evarts Zviedris <125508859+MartinsEvartsZviedris221RDB210@users.noreply.github.com>
2023-03-05 10:11:55 +0200

    Update tree_height.py

--- tree_height.py
+++ tree_height.py
@@ -1,33 +1,43 @@
-# python3
-
 import sys
 import threading
-import numpy
+import numpy as np
 
 
-def compute_height(n, parents):
-    # Write this function
-    max_height = 0
-    # Your code here
-    return max_height
-
+def computeheight(n, parents):
+    nodes = [[] for  in range(n)]
+    root = None
+    for child, parent in enumerate(parents):
+        if parent == -1:
+            root = child
+        else:
+            nodes[parent].append(child)
+    def height(node):
+        if not nodes[node]:
+            return 1
+        return 1 + max(height(child) for child in nodes[node])
+    return height(root)
 
 def main():
-    # implement input form keyboard and from files
-    
-    # let user input file name to use, don't allow file names with letter a
-    # account for github input inprecision
-    
-    # input number of elements
-    # input values in one variable, separate with space, split these values in an array
-    # call the function and output it's result
-    pass
 
-# In Python, the default limit on recursion depth is rather low,
-# so raise it here for this problem. Note that to take advantage
-# of bigger stack, we have to launch the computation in a new thread.
-sys.setrecursionlimit(10**7)  # max depth of recursion
-threading.stack_size(2**27)   # new thread will get stack of such size
+    input_type = input("Ievadiet I lai pats ievadītu skaitļus vai F ja vēlaties no faila ievadīt skaitļus: ")
+    if input_type.upper() == "F":
+        filename = input("Ievadiet faila nosaukumu: ")
+        while "a" in filename:
+            filename = input("Ievadiet faila nosaukumu, kas nesatur burtu 'a': ")
+        with open(f"input/{filename}", "r") as file:
+            n = int(file.readline().strip())
+            parents = list(map(int, file.readline().strip().split()))
+    else:
+        input_str = input("Ievadiet cik jums būs 'nodes' un 'parents of nodes' atdalot ar atstarpi: ")
+        while "a" in input_str:
+            input_str = input("Lūdzu ievadiet vēlreiz, bez burta 'a': ")
+        input_arr = list(map(int, input_str.strip().split()))
+        n = input_arr[0]
+        parents = input_arr[1:]
+    height = compute_height(n, parents)
+    print("Augstums: ", end="")
+    print(np.array(height))
+
+sys.setrecursionlimit(107)
+threading.stack_size(227)
 threading.Thread(target=main).start()
-main()
-# print(numpy.array([1,2,3]))

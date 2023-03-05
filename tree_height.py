9bdf7fa96ba64b5b7b32b540a02a067fa1ef8d1b
Mārtiņš Evarts Zviedris <125508859+MartinsEvartsZviedris221RDB210@users.noreply.github.com>
2023-03-05 17:53:25 +0200

    Update tree_height.py

--- tree_height.py
+++ tree_height.py
@@ -1,43 +1,74 @@
 import sys
 import threading
-import numpy as np
 
 
-def computeheight(n, parents):
-    nodes = [[] for  in range(n)]
-    root = None
-    for child, parent in enumerate(parents):
-        if parent == -1:
-            root = child
+def compute_height(n, parents):
+
+    koks = [[] for _ in range(n)]
+    root_node = None
+
+    for child_node, parent_node in enumerate(parents):
+        
+        if parent_node == -1:
+            root_node = child_node
+            
         else:
-            nodes[parent].append(child)
-    def height(node):
-        if not nodes[node]:
-            return 1
-        return 1 + max(height(child) for child in nodes[node])
-    return height(root)
+            koks[parent_node].append(child_node)
+
+    def garums(node):
+        
+        height = 1
+        
+        if not koks[node]:
+            
+            return height
+        
+        else:
+            for child in koks[node]:
+                
+                height = max(height, garums(child))
+
+            return height + 1
+        
+    return garums(root_node)
+
 
 def main():
+    
+    input_type = input("Ievadiet 'I' ja vēlaties skaitļus ievadīt no tastatūras vai 'F' ja vēlaties no faila ievadīt: ")
 
-    input_type = input("Ievadiet I lai pats ievadītu skaitļus vai F ja vēlaties no faila ievadīt skaitļus: ")
-    if input_type.upper() == "F":
-        filename = input("Ievadiet faila nosaukumu: ")
-        while "a" in filename:
-            filename = input("Ievadiet faila nosaukumu, kas nesatur burtu 'a': ")
-        with open(f"input/{filename}", "r") as file:
-            n = int(file.readline().strip())
-            parents = list(map(int, file.readline().strip().split()))
+    if input_type == "I":
+        
+        n = int(input("ievadiet 'nodes' daudzumu: "))
+        parents = list(map(int, input("ievadiet vērtības atdalot ar atstarpi: ").split()))
+       
+    elif input_type == "F":
+        
+        file_name = input("ievadiet faila nosaukumu: ")
+        
+        if "a" in file_name:
+            
+            print("faila nosaukums nedr;ikst saturēt burtu 'a'")
+            return
+        
+        try:
+            
+            with open(f"./test/{file_name}", "r") as f:
+                n = int(f.readline())
+                parents = list(map(int, f.readline().split()))
+                
+        except Exception as e:
+            print(f"Error: {e}")
+            return
+        
     else:
-        input_str = input("Ievadiet cik jums būs 'nodes' un 'parents of nodes' atdalot ar atstarpi: ")
-        while "a" in input_str:
-            input_str = input("Lūdzu ievadiet vēlreiz, bez burta 'a': ")
-        input_arr = list(map(int, input_str.strip().split()))
-        n = input_arr[0]
-        parents = input_arr[1:]
-    height = compute_height(n, parents)
-    print("Augstums: ", end="")
-    print(np.array(height))
+        print("Error: Invalid input type.")
+        return
 
-sys.setrecursionlimit(107)
-threading.stack_size(227)
+    print(compute_height(n, parents))
+
+
+# Increase recursion depth limit and stack size for larger inputs.
+sys.setrecursionlimit(10**7)
+threading.stack_size(2**27)
 threading.Thread(target=main).start()
